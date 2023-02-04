c0ec2dc829e98fd90fe5e1581cd8c0f8a9e5440b
Harison <Uleborharison@gmail.com>
2023-02-04 21:53:54 +0100

    nest the star component insinde the product-list component as its child component

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
-  styleUrls: ['./star.component.css']
+  styleUrls: ['./star.component.css'],
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnInit, OnChanges {
+  rating: number = 4;
+  cropWidth: number = 75;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {}
+  ngOnChanges(): void {
+    this.cropWidth = (this.rating * 75) / 5;
   }
 
+  ngOnInit(): void {}
 }
